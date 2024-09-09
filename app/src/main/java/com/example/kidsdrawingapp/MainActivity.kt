d7dbddf73eaf73e50fdb35ac7cbfc57990abff07
dev-sharma3624 <devsharma.3624@gmail.com>
2024-09-09 19:04:02 +0530

    Created save image feature plus pixel 7 pro permission denied bug not fixed

--- app/src/main/java/com/example/kidsdrawingapp/MainActivity.kt
+++ app/src/main/java/com/example/kidsdrawingapp/MainActivity.kt
@@ -5,9 +5,15 @@ import android.app.AlertDialog
 import android.app.Dialog
 import android.app.appsearch.StorageInfo
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Log
 import android.view.View
+import android.widget.FrameLayout
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.LinearLayout
@@ -18,6 +24,13 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
 
 class MainActivity : AppCompatActivity() {
 
@@ -44,6 +57,8 @@ class MainActivity : AppCompatActivity() {
                 val isGranted = it.value
 
                 if(isGranted){
+                    Log.d("PERMISSION", "inside if block of request permission")
+
                     Toast.makeText(
                         this,
                         "Permission granted for external storage",
@@ -55,6 +70,8 @@ class MainActivity : AppCompatActivity() {
                     openGalleryLauncher.launch(pickIntent)
 
                 }else{
+                    Log.d("PERMISSION", "inside else block of request permission")
+
                     if(permissionName == Manifest.permission.READ_EXTERNAL_STORAGE){
                         Toast.makeText(
                             this,
@@ -87,8 +104,20 @@ class MainActivity : AppCompatActivity() {
             drawingView?.onClickUndo()
         }
 
+        val ib_save: ImageButton = findViewById(R.id.ib_save)
+        ib_save.setOnClickListener {
+            if(isReadStorageAllowed()){
+                lifecycleScope.launch {
+                    val flDrawingView: FrameLayout = findViewById(R.id.fl_drawing_view_container)
+
+                    saveBitmapFile(getBitmapFromView(flDrawingView))
+                }
+            }
+        }
+
         val ibGallery : ImageButton = findViewById(R.id.ib_gallery)
         ibGallery.setOnClickListener {
+            Log.d("PERMISSION", "calling requestStoragePermission")
             requestStoragePermission()
         }
 
@@ -139,20 +168,102 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun isReadStorageAllowed(): Boolean{
+        val result = ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.READ_EXTERNAL_STORAGE
+        )
+
+        return result == PackageManager.PERMISSION_GRANTED
+    }
+
     private fun requestStoragePermission(){
         if(ActivityCompat.shouldShowRequestPermissionRationale(
             this,
             Manifest.permission.READ_EXTERNAL_STORAGE)
         ){
+
+            Log.d("PERMISSION", "inside if block of request storage permission")
+
             showRationaleDialog(
                 "Kids Drawing App",
                 "Kids Drawing App needs to access your external storage"
             )
         }else{
+            Log.d("PERMISSION", "inside else block of request storage permission")
+
             requestPermission.launch(arrayOf(
-                Manifest.permission.READ_EXTERNAL_STORAGE
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
             ))
         }
+    }
+
+    private fun getBitmapFromView(view: View): Bitmap {
+        val returnedBitmap = Bitmap.createBitmap(
+            view.width,
+            view.height,
+            Bitmap.Config.ARGB_8888
+        )
+
+        val canvas = Canvas(returnedBitmap)
+        val bgDrawable = view.background
+
+        if(bgDrawable != null){
+            bgDrawable.draw(canvas)
+        }else{
+            canvas.drawColor(Color.WHITE)
+        }
+
+        view.draw(canvas)
+
+        return returnedBitmap
+    }
+
+    private suspend fun saveBitmapFile(mBitmap: Bitmap?): String{
+        var result = ""
+        withContext(Dispatchers.IO){
+            if(mBitmap != null){
+                try{
+                    val bytes =  ByteArrayOutputStream()
+                    mBitmap.compress(
+                        Bitmap.CompressFormat.PNG,
+                        90,
+                        bytes
+                    )
+
+                    val f = File(externalCacheDir?.absoluteFile.toString()
+                            + File.separator + "KidsDrawingApp_" + System.currentTimeMillis()/1000 + ".png"
+                    )
+
+                    val fo = FileOutputStream(f)
+                    fo.write(bytes.toByteArray())
+                    fo.close()
+
+                    result = f.absolutePath
+
+                    runOnUiThread {
+                        if(result.isNotEmpty()){
+                            Toast.makeText(
+                                this@MainActivity,
+                                "File save successfully: $result",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }else{
+                            Toast.makeText(
+                                this@MainActivity,
+                                "Something went wrong saving the file",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+                }catch (e: Exception){
+                    result = ""
+                    e.printStackTrace()
+                }
+            }
+        }
+        return result
     }
 
 
